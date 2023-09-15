bd8159b09aa02a5392c8bec48dfdabf04e17fe38
AndrewLester <alester220@gmail.com>
2023-09-15 16:20:52 -0500

    Add new SUPER important React stuff

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -1,6 +1,24 @@
 import "./App.css";
 
-// [!] Create the "App" component here and all the necessary functionality.
-//     Then, uncomment the "export" line at the bottom
+function App() {
+  return (
+    <div>
+      Check out my code. wow <p>no one has ever created anything as cool</p>
+      <p>As this</p>
+      <img src="oops"></img>
+      <code>This html doesn't look so good</code>
+      <b>
+        <b>
+          <b>
+            <b>
+              <b>tags</b>
+            </b>
+          </b>
+        </b>
+      </b>
+      <iframe src="/">Wait does this work? This could be sick</iframe>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
