902e1ca99781a97cf2ea4f46435d8ec1b5567de5
AndrewLester <alester220@gmail.com>
2023-09-14 19:35:42 -0500

    Fix

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -1,6 +1,49 @@
+import { useRef, useState } from "react";
 import "./App.css";
 
-// [!] Create the "App" component here and all the necessary functionality.
-//     Then, uncomment the "export" line at the bottom
+function App() {
+  const [contributions, setContributions] = useState([]);
+  const dialog = useRef();
 
-// export default App;
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    const name = formData.get("name");
+
+    setContributions((contributions) => [...contributions, { name }]);
+
+    form.reset();
+    dialog.close();
+  };
+
+  return (
+    <>
+      <button id="contribute-button" onClick={() => dialog.showModal()}>
+        + Contribute
+      </button>
+
+      <dialog id="contribute-dialog" ref={dialog}>
+        <form id="contribute-form" onSubmit={onFormSubmit}>
+          <label for="contribute-form-name">Name:</label>
+          <input
+            id="contribute-form-name"
+            placeholder="Yummy food"
+            name="name"
+          />
+          <button>Submit</button>
+        </form>
+      </dialog>
+
+      <div>
+        {contributions.map((contribution) => (
+          <div>{contribution.name}</div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
